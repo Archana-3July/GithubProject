4c2e33a0f9ad1a480be9c134184dd7a2a1ef1212
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-17 15:27:53 +0530

    print message of Bank manager

--- src/test/java/test_Class/TC003_Search_Customer.java
+++ src/test/java/test_Class/TC003_Search_Customer.java
@@ -12,6 +12,7 @@ public class TC003_Search_Customer extends BaseClass{
         driver.get(url);
         Add_Customer AC = new Add_Customer(driver);
         AC.Click_BankManagerLogin();
+        System.out.println("Bank Manager Login is clicked..");
         Search_Customer SC = new Search_Customer(driver);
         SC.Click_Customer();
         SC.Add_Value_Search();

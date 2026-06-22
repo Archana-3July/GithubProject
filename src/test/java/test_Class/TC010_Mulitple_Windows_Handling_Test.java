f349140e96b1ec734945adc73ff30ded07785a34
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-22 17:35:38 +0530

    windows updated

--- src/test/java/test_Class/TC010_Mulitple_Windows_Handling_Test.java
+++ src/test/java/test_Class/TC010_Mulitple_Windows_Handling_Test.java
@@ -7,10 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
 import java.time.Duration;
-import java.util.Iterator;
-import java.util.Set;
 
 public class TC010_Mulitple_Windows_Handling_Test {
     public static void main(String[] args) throws InterruptedException {

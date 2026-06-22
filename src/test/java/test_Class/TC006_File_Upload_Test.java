e0adea45ec4dfe56df6c207982d69f6424e4ab18
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-22 10:18:59 +0530

    update Upload File

--- src/test/java/test_Class/TC006_File_Upload_Test.java
+++ src/test/java/test_Class/TC006_File_Upload_Test.java
@@ -4,19 +4,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import java.io.File;
 import java.time.Duration;
 
 public class TC006_File_Upload_Test {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
-        driver.get("https://the-internet.herokuapp.com/");
-        driver.findElement(By.xpath("//a[text()='File Upload']")).click();
-        WebElement ChooseFile = driver.findElement(By.xpath("//input[@id='file-upload']"));
-        //ChooseFile.click();
-        ChooseFile.sendKeys("C:\\Users\\Archana\\Documents\\test.txt");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+        driver.get("https://the-internet.herokuapp.com/upload");
+        File uploadFile = new File("src/main/resources/test-upload.txt");
+
+        WebElement fileInput = driver.findElement(By.cssSelector("input[type=file]"));
+        fileInput.sendKeys(uploadFile.getAbsolutePath());
         driver.findElement(By.id("file-submit")).click();
+        driver.quit();
     }
 }

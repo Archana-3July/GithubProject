2e097187e95ad48bab4c29d6a2c9bbdafd88c411
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-22 14:41:25 +0530

    Multiple window are added test script

---
+++ src/test/java/test_Class/TC010_Mulitple_Windows_Handling_Test.java
@@ -0,0 +1,48 @@
+package test_Class;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+public class TC010_Mulitple_Windows_Handling_Test {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4000));
+        driver.manage().window().maximize();
+        String MainWindowID = driver.getWindowHandle();
+        driver.switchTo().window(MainWindowID);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[text()='Dropdown menu']")).click();
+        Actions act=new Actions(driver);
+        WebElement UseClickButton = driver.findElement(By.id("my-dropdown-1"));
+        act.click(UseClickButton).build().perform();
+        Thread.sleep(2000);
+        WebElement Right_Click = driver.findElement(By.xpath("//button[@id='my-dropdown-2']"));
+        act.contextClick(Right_Click).build().perform();
+        Thread.sleep(2000);
+        WebElement Double_Click = driver.findElement(By.xpath("//button[@id='my-dropdown-3']"));
+        act.doubleClick(Double_Click).build().perform();
+        driver.switchTo().window(MainWindowID);
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+
+
+
+
+
+
+
+
+
+}

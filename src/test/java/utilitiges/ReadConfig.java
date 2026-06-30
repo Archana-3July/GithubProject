9f004a62f1e9cc5e29b93abf508e6ff9559fddb9
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-30 11:52:51 +0530

    update ReadConfig file

--- src/test/java/utilitiges/ReadConfig.java
+++ src/test/java/utilitiges/ReadConfig.java
@@ -41,4 +41,41 @@ public class ReadConfig {
         else
             throw new RuntimeException("Browser not specified in config file");
     }
+
+    public String getFirstName(){
+        String value= properties.getProperty("firstName");
+        if(value!=null)
+            return value;
+        else
+            throw new RuntimeException("First name not specified in config file");
+    }
+    public String getLastName(){
+        String value= properties.getProperty("lastName");
+        if(value!=null)
+            return value;
+        else
+            throw new RuntimeException("Last name not specified in config file");
+    }
+    public String getPostCode(){
+        String value= properties.getProperty("postCode");
+        if(value!=null)
+            return value;
+        else
+            throw new RuntimeException("Post Code not specified in config file");
+    }
+
+    public String getCustomerName(){
+        String value= properties.getProperty("customerName");
+        if(value!=null)
+            return value;
+        else
+            throw new RuntimeException("Customer Name not specified in config file");
+    }
+    public String getCurrency(){
+        String value= properties.getProperty("currency");
+        if(value!=null)
+            return value;
+        else
+            throw new RuntimeException("Currency not specified in config file");
+    }
 }

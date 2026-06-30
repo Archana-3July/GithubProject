6311700569f812b0d3e422159e3507d4c3af4aa0
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-30 11:47:40 +0530

    update TC005_File_Download file

--- src/test/java/test_Class/TC005_File_Download_Test.java
+++ src/test/java/test_Class/TC005_File_Download_Test.java
@@ -2,14 +2,17 @@ package test_Class;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import java.io.File;
 
 public class TC005_File_Download_Test{
-    public static void main(String[] args){
+    @Test
+    public void File_Download(){
          WebDriver driver=new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.xpath("//a[text()='File Download']")).click();
-           driver.findElement(By.xpath("//a[text()='sample-zip-file.zip']")).click();
+           driver.findElement(By.xpath("//a[text()='sample-upload.txt']")).click();
         File file = new File("C:\\Users\\Archana Satpute\\Downloads\\sample-zip-file.zip");
         if(file.exists())
         {

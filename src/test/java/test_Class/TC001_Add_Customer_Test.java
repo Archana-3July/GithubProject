3e10e719fc77a5b59298d6b31b8a456af53bd085
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-06-30 11:43:54 +0530

    update TC001_Add_Customer file

--- src/test/java/test_Class/TC001_Add_Customer_Test.java
+++ src/test/java/test_Class/TC001_Add_Customer_Test.java
@@ -3,22 +3,21 @@ import org.testng.annotations.Test;
 
  import pom_Class.Add_Customer;
 
+import java.time.Duration;
+
 public class TC001_Add_Customer_Test extends BaseClass{
      @Test
 	public void verify_AddCustomer()
 	{
 		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
 		Add_Customer AC=new Add_Customer(driver);
 		AC.Click_BankManagerLogin();
 		AC.Click_AddCustomerMenu();
-		AC.Add_FirstName("ABC");
-		AC.Add_LastName("XYZ");
-		AC.Add_PostCode("415110");
+		AC.Add_FirstName(firstName);
+		AC.Add_LastName(lastName);
+		AC.Add_PostCode(postCode);
 		AC.Click_AddCustomer();
-
+		System.out.println("Added successfully");
 	}
-
-
-
-
 }

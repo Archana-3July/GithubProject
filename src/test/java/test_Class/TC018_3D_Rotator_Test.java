83fe66876008bf97edd7f16853a3881ce8a11f14
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-07-17 12:47:42 +0530

    update Rotator

--- src/test/java/test_Class/TC018_3D_Rotator_Test.java
+++ src/test/java/test_Class/TC018_3D_Rotator_Test.java
@@ -1,8 +1,13 @@
 package test_Class;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -13,14 +18,21 @@ public class TC018_3D_Rotator_Test {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.jqueryscript.net/demo/3d-rotator-jascarousel/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
         for(int i=0;i<=15;i++) {
             Thread.sleep(100);
-            driver.findElement(By.xpath("//a[@class='next']")).click();
+            WebElement next=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='next']")));
+            ((JavascriptExecutor) driver)
+                    .executeScript("arguments[0].click();", next);
+            Thread.sleep(300);
         }
         for(int i=0;i<=15;i++) {
             Thread.sleep(100);
-            driver.findElement(By.xpath("//a[@class='prev']")).click();
+            WebElement prev = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='prev']")));
+            ((JavascriptExecutor) driver)
+                    .executeScript("arguments[0].click();", prev);
+            Thread.sleep(300);
         }
         driver.quit();
     }

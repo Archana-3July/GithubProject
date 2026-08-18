47e099809e16cf7a09a194a84ae1f6636738a761
Archana.Satpute <archanaamolsatpute@gmail.com>
2026-08-18 11:55:52 +0530

    Added new script of Reset Account

--- src/main/java/pom_Class/Reset_Customer.java
+++ src/main/java/pom_Class/Reset_Customer.java
@@ -1,4 +1,26 @@
 package pom_Class;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
 public class Reset_Customer {
+    WebDriver ldriver;
+    public Reset_Customer(WebDriver rdriver){
+        ldriver=rdriver;
+        PageFactory.initElements(rdriver,this);
+    }
+    @FindBy(xpath="//select[@id='accountSelect']")
+    WebElement SelectAccount;
+    @FindBy(xpath="//button[text()='Reset']") WebElement Reset_Button;
+   public void verfiy_Select_AccNo(){
+    Select s=new Select(SelectAccount);
+       s.selectByVisibleText("1003");
+  }
+    public void Click_Reset_Button(){
+       Reset_Button.click();
+        System.out.println("Customer Account Reset Successfully");
+    }
 }
--- src/test/java/test_Class/TC030_Reset_Customer_Account_Test.java
+++ src/test/java/test_Class/TC030_Reset_Customer_Account_Test.java
@@ -1,4 +1,34 @@
 package test_Class;
 
-public class TC030_Reset_Customer_Account_Test {
+import org.testng.annotations.Test;
+import pom_Class.*;
+import java.time.Duration;
+
+public class TC030_Reset_Customer_Account_Test extends BaseClass{
+    @Test
+    public void Verify_Reset_Customer(){
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2000));
+        Customer_Login CL=new Customer_Login(driver);
+        CL.Click_Customer_Login();
+        CL.Select_User_Name();
+        CL.Click_Submit();
+        Reset_Customer RC=new Reset_Customer(driver);
+        RC.verfiy_Select_AccNo();
+        Deposit_Customer DC = new Deposit_Customer(driver);
+        DC.Click_DepositeMenu();
+        DC.Add_Deposite();
+        DC.Click_DepositeButton();
+        DC.Add_Deposite();
+        DC.Click_DepositeButton();
+        DC.Add_Deposite();
+        DC.Click_DepositeButton();
+        DC.Add_Deposite();
+        DC.Click_DepositeButton();
+        Customer_Login_Trasction CLT =new Customer_Login_Trasction(driver);
+        CLT.Click_Transaction();
+        RC.Click_Reset_Button();
+    }
+
 }
